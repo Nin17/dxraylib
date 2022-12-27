fdf577d43c3e8de2d10d45f7bb5c20a94cb024f2
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2022-12-27 21:52:24 +0000

    import cs_kn

--- pyxraylib/__init__.py
+++ pyxraylib/__init__.py
@@ -4,7 +4,14 @@
 from .anomalous_scattering import fi, fii
 from .atomic_weight import atomic_weight
 from .compound_parser import compound_parser
-from .cross_sections import cs_compt, cs_energy, cs_photo, cs_rayl, cs_total
+from .cross_sections import (
+    cs_compt,
+    cs_energy,
+    cs_kn,
+    cs_photo,
+    cs_rayl,
+    cs_total,
+)
 from .refractive_indices import (
     refractive_index,
     refractive_index_im,
@@ -17,6 +24,7 @@ __all__ = (
     "compound_parser",
     "cs_compt",
     "cs_energy",
+    "cs_kn",
     "cs_photo",
     "cs_rayl",
     "cs_total",
