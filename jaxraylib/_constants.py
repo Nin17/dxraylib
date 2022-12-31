4b52e10c95d904645f8374f20fd211a35cbfd182
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2022-12-31 22:19:37 +0000

    add kev to angst conversion

--- jaxraylib/_constants.py
+++ jaxraylib/_constants.py
@@ -4,5 +4,6 @@
 KD = 4.15179082788e-4
 HC_4PI = 9.8663479e-9  # ??? h*c/4pi apparently ???
 AVOGNUM = 0.602214129  # Avogadro number (mol-1 * barn-1 * cm2)
+KEV2ANGST = 12.39841930  # keV to angstrom-1 conversion factor
 MEC2 = 511.0034  # electron rest mass (keV)
 RE2 = 0.07940775  # square of classical electron radius (barn)
