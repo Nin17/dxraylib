ecacdc466a0662c52b23d8ec3cc79a60a78b3ad7
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-01-01 12:01:51 +0000

    type hinting, jit with errors

--- jaxraylib/fi.py
+++ jaxraylib/fi.py
@@ -1,7 +1,12 @@
-"""_summary_
 """
-# TODO docstrings
+Anomalous Scattering Factor Fi
+"""
+
+from __future__ import annotations
+import functools
 import os
+
+from jax._src.typing import Array
 
 from .config import jit, xp
 from ._splint import _splint
@@ -9,40 +14,42 @@ from ._splint import _splint
 DIRPATH = os.path.dirname(__file__)
 FI_PATH = os.path.join(DIRPATH, "data/fi.npy")
 FI = xp.load(FI_PATH)
+shape = FI.shape
+VALUE_ERROR = f"Z out of range: 1 to {shape}"
 
 
-@jit
-def _Fi(Z: int, E: float) -> float:
-    """_summary_
-
-    Parameters
-    ----------
-    Z : int
-        _description_
-    E : float
-        _description_
-
-    Returns
-    -------
-    float
-        _description_
-    """
+@functools.partial(
+    jit, **({"static_argnums": (0, 1)} if jit.__name__ == "jit" else {})
+)
+def _Fi(Z: int, E: float) -> Array | float:
+    if Z < 1 or Z > 99:
+        raise ValueError(VALUE_ERROR)
+    if E < 0:
+        raise ValueError("negative energy")
     return _splint(FI[Z - 1], E)
 
 
-def Fi(Z: int, E: float) -> float:
-    """_summary_
+def Fi(Z: int, E: Array | float) -> Array | float:
+    """
+    Anomalous Scattering Factor Fi
 
     Parameters
     ----------
     Z : int
-        _description_
+        atomic number
     E : float
-        _description_
+        Energy (keV)
 
     Returns
     -------
-    float
-        _description_
+    Array | float
+        Anomalous Scattering Factor Fi
+
+    Raises
+    ------
+    ValueError
+        if Z < 1 or Z > 99
+    ValueError
+        if E < 0
     """
     return _Fi(Z, E)
--- jaxraylib/fii.py
+++ jaxraylib/fii.py
@@ -1,8 +1,12 @@
-"""_summary_
 """
-# TODO docstrings
+Anomalous Scattering Factor Fii
+"""
 
+from __future__ import annotations
+import functools
 import os
+
+from jax._src.typing import Array
 
 from .config import jit, xp
 from ._splint import _splint
@@ -10,42 +14,42 @@ from ._splint import _splint
 DIRPATH = os.path.dirname(__file__)
 FII_PATH = os.path.join(DIRPATH, "data/fii.npy")
 FII = xp.load(FII_PATH)
+shape = FII.shape[0]
+VALUE_ERROR = f"Z out of range: 1 to {shape}"
 
-del DIRPATH, FII_PATH
 
-
-@jit
-def _Fii(Z: int, E: float) -> float:
-    """_summary_
-
-    Parameters
-    ----------
-    Z : int
-        _description_
-    E : float
-        _description_
-
-    Returns
-    -------
-    float
-        _description_
-    """
+@functools.partial(
+    jit, **({"static_argnums": (0,)} if jit.__name__ == "jit" else {})
+)
+def _Fii(Z: int, E: float) -> Array | float:
+    if Z < 1 or Z > 99:
+        raise ValueError(VALUE_ERROR)
+    if E < 0:
+        raise ValueError("negative energy")
     return _splint(FII[Z - 1], E)
 
 
-def Fii(Z: int, E: float) -> float:
-    """_summary_
+def Fii(Z: int, E: float) -> Array | float:
+    """
+    Anomalous Scattering Factor Fii
 
     Parameters
     ----------
     Z : int
-        _description_
+        atomic number
     E : float
-        _description_
+        Energy (keV)
 
     Returns
     -------
-    float
-        _description_
+    Array | float
+        Anomalous Scattering Factor Fii
+
+    Raises
+    ------
+    ValueError
+        if Z < 1 or Z > 99
+    ValueError
+        if E < 0
     """
     return _Fii(Z, E)
