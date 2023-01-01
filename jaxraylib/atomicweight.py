88cdc44f864dd2699259e2275907243a9d6b47b0
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-01-01 11:06:39 +0000

    jit compile with errors, docstring

--- jaxraylib/atomicweight.py
+++ jaxraylib/atomicweight.py
@@ -1,22 +1,35 @@
-"""_summary_
+"""
+Standard Atomic Weight
 """
 
+from __future__ import annotations
 import os
+import functools
+
+# TODO import this from config
+from jax._src.typing import Array
 
 from .config import jit, xp
 
 DIRPATH = os.path.dirname(__file__)
-
-FI_PATH = os.path.join(DIRPATH, "data/atomic_weight.npy")
-
-data = xp.load(FI_PATH)
-
-del DIRPATH, FI_PATH
+AW_PATH = os.path.join(DIRPATH, "data/atomic_weight.npy")
+data = xp.load(AW_PATH)
+shape = data.shape[0]
+VALUE_ERROR = f"Z out of range: 1 to {shape}"
 
 
-@jit
-def AtomicWeight(Z: int) -> float:
-    """_summary_
+@functools.partial(
+    jit, **({"static_argnums": (0,)} if jit.__name__ == "jit" else {})
+)
+def _AtomicWeight(Z: int) -> float | Array:
+    if Z < 1 or Z > shape:
+        raise ValueError(VALUE_ERROR)
+    return data[Z - 1]
+
+
+def AtomicWeight(Z: int) -> float | Array:
+    """
+    Standard atomic weight
 
     Parameters
     ----------
@@ -25,9 +38,12 @@ def AtomicWeight(Z: int) -> float:
 
     Returns
     -------
-    float
+    float | Array
         standard atomic weight
+
+    Raises
+    ------
+    ValueError
+        if atomic number < 1 or > 103
     """
-    # if Z < 1 or Z > 103:
-    #     raise ValueError("Z out of range: 1 to 103")
-    return data[Z - 1]
+    return _AtomicWeight(Z)
