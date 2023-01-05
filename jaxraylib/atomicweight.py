2fc74c68bf1a5deb0ed984628e0f15e9fdd946af
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-01-05 17:54:35 +0000

    change to match densities.py

--- jaxraylib/atomicweight.py
+++ jaxraylib/atomicweight.py
@@ -3,42 +3,49 @@ Standard Atomic Weight
 """
 
 from __future__ import annotations
-import functools
 import os
 
-# TODO import this from config
-from jax._src.typing import Array
-
-from .config import jit, xp
+from .config import jit, xp, NDArray
+from ._utilities import raise_errors
 
 DIRPATH = os.path.dirname(__file__)
 AW_PATH = os.path.join(DIRPATH, "data/atomic_weight.npy")
 AW = xp.load(AW_PATH)
-shape = AW.shape[0]
-VALUE_ERROR = f"Z out of range: 1 to {shape}"
 
 
-@functools.partial(
-    jit, **({"static_argnums": (0,)} if jit.__name__ == "jit" else {})
-)
-def _AtomicWeight(Z: int) -> Array | float:
-    if Z < 1 or Z > shape:
-        raise ValueError(VALUE_ERROR)
-    return AW[Z - 1]
-
-
-def AtomicWeight(Z: int) -> Array | float:
+@jit
+def _AtomicWeight(Z: int | NDArray) -> tuple[NDArray, bool]:
     """
     Standard atomic weight
 
     Parameters
     ----------
-    Z : int
+    Z : int | Array
+        _description_
+
+    Returns
+    -------
+    tuple[Array, bool]
+        _description_
+    """
+    Z = xp.atleast_1d(xp.asarray(Z))
+    output = xp.where((Z >= 1) & (Z <= AW.shape[0]), AW[Z - 1], xp.nan)
+    return output, xp.isnan(output).any()
+
+
+@raise_errors(f"Z out of range: 1 to {AW.shape[0]}")
+def AtomicWeight(Z: int | NDArray) -> NDArray:
+    """
+    Standard atomic weight
+
+    Parameters
+    ----------
+    Z : int | Array
         atomic number
 
     Returns
     -------
-    Array | float
+    Array
         standard atomic weight
 
     Raises
