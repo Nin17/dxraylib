4514ec8828f2f36581dc5450f808872ba7086579
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-01-12 18:40:35 +0100

    add pi to constants

--- jaxraylib/constants.py
+++ jaxraylib/constants.py
@@ -7,4 +7,5 @@ HC_4PI = 9.8663479e-9  # ??? h*c/4pi apparently ???
 AVOGNUM = 0.602214129  # Avogadro number (mol-1 * barn-1 * cm2)
 KEV2ANGST = 12.39841930  # keV to angstrom-1 conversion factor
 MEC2 = 511.0034  # electron rest mass (keV)
+PI = 3.1415926535897932384626433832795
 RE2 = 0.07940775  # square of classical electron radius (barn)
