408c20336e8cdfffd0eb1d3b8b9b8eae04692e95
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-01-12 19:32:59 +0100

    total interaction cross sections # TODO use new decorators

--- jaxraylib/cross_sections.py
+++ jaxraylib/cross_sections.py
@@ -1,164 +1,322 @@
 """_summary_
 """
 # TODO cross sections in other units
+# TODO redo with xp.where n stuff
+# TODO issue with E = 1
+from __future__ import annotations
 import os
+from typing import overload
 
-from .config import jit, xp
+from .config import jit, jit_kwargs, xp, NDArray
 from ._splint import _splint
+from ._utilities import value_error, wrapped_partial, output_type
 
-MEC2 = 511.0034  # electron rest mass (keV)
-RE2 = 0.07940775  # square of classical electron radius (barn)
 PI = xp.pi
 
 DIRPATH = os.path.dirname(__file__)
 
 CS_COMPT_PATH = os.path.join(DIRPATH, "data/cs_compt.npy")
+CS_ENERGY_PATH = os.path.join(DIRPATH, "data/cs_energy.npy")
 CS_PHOTO_PATH = os.path.join(DIRPATH, "data/cs_photo.npy")
 CS_RAYL_PATH = os.path.join(DIRPATH, "data/cs_rayl.npy")
 
 CS_COMPT = xp.load(CS_COMPT_PATH)
+CS_ENERGY = xp.load(CS_ENERGY_PATH)
 CS_PHOTO = xp.load(CS_PHOTO_PATH)
 CS_RAYL = xp.load(CS_RAYL_PATH)
 
-del DIRPATH, CS_COMPT_PATH, CS_PHOTO_PATH, CS_RAYL_PATH
 
-
-@jit
-def CS_Compt(Z: int, E: float) -> float:
-    """_summary_
-
-    Parameters
-    ----------z
-    Z : int
-        _description_
-    E : float
-        _description_
-
-    Returns
-    -------
-    float
-        _description_
+@wrapped_partial(jit, **jit_kwargs)
+def _CS_Total(
+    Z: int | NDArray[int], E: float | NDArray[float]
+) -> tuple[NDArray[float, bool]]:
     """
-    return xp.exp(_splint(CS_COMPT[Z - 1], xp.log(E * 1000.0)))
-
-
-def CS_Energy():
-    # !!! Not needed
-    return
-
-
-# TODO fix type hint
-@jit
-def _CS_KN(E: float) -> float:
-    """_summary_
+    Total cross section  (cm2/g)
+    (Photoelectric + Compton + Rayleigh)
 
     Parameters
     ----------
-    E : float
-        _description_
+    Z : int | Array
+        atomic number
+    E : float | Array
+        energy (keV)
 
     Returns
     -------
-    float
-        _description_
+    float | Array
+        Total cross section  (cm2/g)
+        (Photoelectric + Compton + Rayleigh)
     """
-    a = E / MEC2
-    a3 = a * a * a
-    b = 1 + 2 * a
-    b2 = b * b
-    lb = xp.log(b)
-    sigma = (
-        2
-        * PI
-        * RE2
-        * (
-            (1 + a) / a3 * (2 * a * (1 + a) / b - lb)
-            + 0.5 * lb / a
-            - (1 + 3 * a) / b2
-        )
+    compton = _CS_Compt(Z, E)[0]
+    photo = _CS_Photo(Z, E)[0]
+    rayleigh = _CS_Rayl(Z, E)[0]
+    output = compton + photo + rayleigh
+    return output, xp.isnan(output).any()
+
+
+@overload
+def CS_Total(Z: int, E: float) -> float:
+    ...
+
+
+@overload
+def CS_Total(Z: NDArray[int], E: NDArray[float]) -> NDArray[float]:
+    ...
+
+
+@output_type
+@value_error(" # TODO value error")
+def CS_Total(Z, E):
+    return _CS_Total(Z, E)
+
+
+@wrapped_partial(jit, **jit_kwargs)
+def _CS_Photo(
+    Z: int | NDArray[int], E: float | NDArray[float]
+) -> tuple[NDArray[float], bool]:
+    """
+    Photoelectric absorption cross section  (cm2/g)
+
+    Parameters
+    ----------
+    Z : Array
+        atomic number
+    E : Array
+        energy (keV)
+
+    Returns
+    -------
+    Array
+        Photoelectric absorption cross section  (cm2/g)
+    """
+    Z = xp.atleast_1d(xp.asarray(Z))
+    E = xp.atleast_1d(xp.asarray(E))
+    # TODO change to CS_PHOTO[Z-1] when _splint is broadcast
+    output = xp.where(
+        (Z >= 1) & (Z < CS_PHOTO.shape[0]) & (E >= 0.0),
+        xp.exp(_splint(CS_PHOTO[Z[0] - 1], xp.log(E * 1000.0))),
+        xp.nan,
     )
-    return sigma
+    return output, xp.isnan(output).any()
 
 
-def CS_KN(E: float) -> float:
+@overload
+def CS_Photo(Z: int, E: float) -> float:
+    ...
+
+
+@overload
+def CS_Photo(Z: NDArray[int], E: NDArray[float]) -> NDArray[float]:
+    ...
+
+
+@output_type
+@value_error("# TODO error message")
+def CS_Photo(Z, E):
     """_summary_
 
     Parameters
     ----------
-    E : float
+    Z : int | Array
+        _description_
+    E : float | Array
         _description_
 
     Returns
     -------
-    float
+    float | Array
         _description_
+    """
+    return _CS_Photo(Z, E)
+
+
+@wrapped_partial(jit, **jit_kwargs)
+def _CS_Rayl(
+    Z: int | NDArray[int], E: float | NDArray[float]
+) -> tuple[NDArray[float], bool]:
+    """
+    Rayleigh scattering cross section  (cm2/g)
+
+    Parameters
+    ----------
+    Z : Array
+        atomic number
+    E : Array
+        energy (keV)
+
+    Returns
+    -------
+    Array
+        Rayleigh scattering cross section  (cm2/g)
+    """
+    Z = xp.atleast_1d(xp.asarray(Z))
+    E = xp.atleast_1d(xp.asarray(E))
+    # TODO change to CS_RAYL[Z-1] when _splint is broadcast
+    output = xp.where(
+        (Z >= 1) & (Z < CS_RAYL.shape[0]) & (E >= 0.0),
+        xp.exp(_splint(CS_RAYL[Z[0] - 1], xp.log(E * 1000.0))),
+        xp.nan,
+    )
+    return output, xp.isnan(output).any()
+
+
+@overload
+def CS_Rayl(Z: int, E: float) -> float:
+    ...
+
+
+@overload
+def CS_Rayl(Z: NDArray[int], E: NDArray[float]) -> NDArray[float]:
+    ...
+
+
+@output_type
+@value_error(" # TODO error message")
+def CS_Rayl(Z, E):
+    """_summary_
+
+    Parameters
+    ----------
+    Z : int | Array
+        _description_
+    E : float | Array
+        _description_
+
+    Returns
+    -------
+    float | Array
+        _description_
+    """
+    return _CS_Rayl(Z, E)
+
+
+@wrapped_partial(jit, **jit_kwargs)
+def _CS_Compt(
+    Z: int | NDArray[int], E: float | NDArray[float]
+) -> tuple[NDArray[float], bool]:
+    """
+    Compton scattering cross section  (cm2/g)
+
+    Parameters
+    ----------
+    Z : Array
+        atomic number
+    E : Array
+        energy (keV)
+
+    Returns
+    -------
+    Array
+        Compton scattering cross section  (cm2/g)
+    """
+    Z = xp.atleast_1d(xp.asarray(Z))
+    E = xp.atleast_1d(xp.asarray(E))
+    # TODO change to CS_COMPT[Z-1] when _splint is broadcast
+    output = xp.where(
+        (Z >= 1) & (Z < CS_COMPT.shape[0]) & (E >= 0.0),
+        xp.exp(_splint(CS_COMPT[Z[0] - 1], xp.log(E * 1000.0))),
+        xp.nan,
+    )
+    return output, xp.isnan(output).any()
+
+
+@overload
+def CS_Compt(Z: int, E: float) -> float:
+    ...
+
+
+@overload
+def CS_Compt(Z: NDArray[int], E: NDArray[float]) -> NDArray[float]:
+    ...
+
+
+@output_type
+@value_error(" # TODO value error")
+def CS_Compt(Z, E):
+    # TODO docstring
+    """_summary_
+
+    Parameters
+    ----------
+    Z : int | Array
+        _description_
+    E : float | Array
+        _description_
+
+    Returns
+    -------
+    float | Array
+        _description_
+    """
+    return _CS_Compt(Z, E)
+
+
+@wrapped_partial(jit, **jit_kwargs)
+def _CS_Energy(
+    Z: int | NDArray[int], E: float | NDArray[float]
+) -> tuple[NDArray[float], bool]:
+    # TODO docstring
+    """_summary_
+
+    Parameters
+    ----------
+    Z : int | NDArray[int]
+        _description_
+    E : float | NDArray[float]
+        _description_
+
+    Returns
+    -------
+    tuple[NDArray[float], bool]
+        _description_
+    """
+    Z = xp.atleast_1d(xp.asarray(Z))
+    E = xp.atleast_1d(xp.asarray(E))
+    # TODO change to CS_ENERGY[Z-1] when broadcast _splint
+    output = xp.where(
+        (Z >= 1) & (Z < CS_ENERGY.shape[0]) & (E >= 0.0),
+        _splint(CS_ENERGY[Z[0] - 1], xp.log(E)),
+        xp.nan,
+    )
+    return xp.exp(output), xp.isnan(output).any()
+
+
+@overload
+def CS_Energy(Z: int, E: float) -> float:
+    ...
+
+
+@overload
+def CS_Energy(Z: NDArray[int], E: NDArray[float]) -> NDArray[float]:
+    ...
+
+
+CS_Energy_error = f"""Z out of range: 1 to {CS_ENERGY.shape[0]} |
+ Energy must be strictly positive"""
+
+
+@output_type
+@value_error("".join(CS_Energy_error.splitlines()))
+def CS_Energy(Z, E):
+    """
+    Mass energy-absorption coefficient (cm2/g)
+
+    Parameters
+    ----------
+    Z : int | Array
+        atomic number
+    E : float | Array
+        energy (keV)
+
+    Returns
+    -------
+    float | Array
+        Mass energy-absorption coefficient (cm2/g)
 
     Raises
     ------
     ValueError
+        # TODO finish docstring
         _description_
     """
-    if E < 0:
-        raise ValueError("energy must be positive")
-    return _CS_KN(E)
-
-
-@jit
-def CS_Photo(Z: int, E: float) -> float:
-    """_summary_
-
-    Parameters
-    ----------
-    Z : int
-        _description_
-    E : float
-        _description_
-
-    Returns
-    -------
-    float
-        _description_
-    """
-    return xp.exp(_splint(CS_PHOTO[Z - 1], xp.log(E * 1000.0)))
-
-
-@jit
-def CS_Rayl(Z: int, E: float) -> float:
-    """_summary_
-
-    Parameters
-    ----------
-    Z : int
-        _description_
-    E : float
-        _description_
-
-    Returns
-    -------
-    float
-        _description_
-    """
-    return xp.exp(_splint(CS_RAYL[Z - 1], xp.log(E * 1000.0)))
-
-
-@jit
-def CS_Total(Z: int, E: float) -> float:
-    """_summary_
-
-    Parameters
-    ----------
-    Z : int
-        _description_
-    E : float
-        _description_
-
-    Returns
-    -------
-    float
-        _description_
-    """
-    compton = CS_Compt(Z, E)
-    photo = CS_Photo(Z, E)
-    rayleigh = CS_Rayl(Z, E)
-
-    return compton + photo + rayleigh
+    return _CS_Energy(Z, E)
