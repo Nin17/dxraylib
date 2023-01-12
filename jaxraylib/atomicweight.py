a307a3e02af74b40e011a8b23a3b9c74ce68ee52
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-01-12 16:42:04 +0100

    redo type hints w ArrayLike decorate with xrl_xrlnp

--- jaxraylib/atomicweight.py
+++ jaxraylib/atomicweight.py
@@ -4,10 +4,9 @@ Standard Atomic Weight
 
 from __future__ import annotations
 import os
-from typing import overload
 
-from .config import jit, jit_kwargs, xp, NDArray
-from ._utilities import value_error, wrapped_partial, output_type
+from .config import jit, jit_kwargs, xp, NDArray, ArrayLike
+from ._utilities import wrapped_partial, xrl_xrlnp
 
 DIRPATH = os.path.dirname(__file__)
 AW_PATH = os.path.join(DIRPATH, "data/atomic_weight.npy")
@@ -15,54 +14,52 @@ AW = xp.load(AW_PATH)
 
 
 @wrapped_partial(jit, **jit_kwargs)
-def _AtomicWeight(Z: int | NDArray[int]) -> tuple[NDArray[float], bool]:
+def _AtomicWeight(Z: ArrayLike) -> tuple[NDArray[float], bool]:
     """
     Standard atomic weight
 
     Parameters
     ----------
-    Z : int | Array
-        _description_
+    Z : array_like
+        atomic number
 
     Returns
     -------
-    tuple[Array, bool]
-        _description_
+    tuple[Array[float], bool]
+
+
+
     """
     Z = xp.atleast_1d(xp.asarray(Z))
     output = xp.where((Z >= 1) & (Z <= AW.shape[0]), AW[Z - 1], xp.nan)
     return output, xp.isnan(output).any()
 
 
-@overload
-def AtomicWeight(Z: int) -> float:
-    ...
-
-
-@overload
-def AtomicWeight(Z: NDArray[int]) -> NDArray[float]:
-    ...
-
-
-@output_type
-@value_error(f"Z out of range: 1 to {AW.shape[0]}")
-def AtomicWeight(Z):
+# TODO another version with jax jitable decorator
+@xrl_xrlnp(f"Z out of range: 1 to {AW.shape[0]}")
+def AtomicWeight(Z: ArrayLike) -> NDArray[float]:
     """
     Standard atomic weight
 
     Parameters
     ----------
-    Z : int | Array
+    Z : array_like
         atomic number
 
     Returns
     -------
-    float | Array
+    Array
         standard atomic weight
 
     Raises
     ------
     ValueError
         if atomic number < 1 or > 103
+
+    Notes
+    ------
+    Z must be <= 1D if jaxraylib.config.jit == numba.njit
+
+
     """
     return _AtomicWeight(Z)
