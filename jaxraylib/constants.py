06a05769d82e776b2bdb9d1e78f8e2ae3dbd9c44
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-03-08 15:03:38 +0100

    alphabetical

--- jaxraylib/constants.py
+++ jaxraylib/constants.py
@@ -2,10 +2,14 @@
 Physical constants and conversion factors
 """
 
-KD = 4.15179082788e-4
-HC_4PI = 9.8663479e-9  # ??? h*c/4pi apparently ???
 AVOGNUM = 0.602214129  # Avogadro number (mol-1 * barn-1 * cm2)
+HC_4PI = 9.8663479e-9  # ??? h*c/4pi apparently ???
 KEV2ANGST = 12.39841930  # keV to angstrom-1 conversion factor
-MEC2 = 511.0034  # electron rest mass (keV)
+KD = 4.15179082788e-4
+MEC2 = 510.998950  # electron rest mass (keV)
+# MEC2 = 511.0034  # ??? Multiple definitions
+# MEC2 = 510.998928 # ???
+# MEC2 = 510.998910  # ???
 PI = 3.1415926535897932384626433832795
 RE2 = 0.07940775  # square of classical electron radius (barn)
+# RE2 = 0.079407877 # ???
