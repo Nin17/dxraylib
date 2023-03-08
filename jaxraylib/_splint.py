80a6ef6406c8c1c4f175874a46fd7c72790b1700
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-03-08 15:03:23 +0100

    broadcast to work with any input

--- jaxraylib/_splint.py
+++ jaxraylib/_splint.py
@@ -1,78 +1,59 @@
-"""_summary_
 """
-
+Cubic spline interpolation from x, y & y'' data
+"""
+# TODO docstring
+# TODO compatibility with numba
 from __future__ import annotations
 
-from jax._src.typing import Array
+from ._utilities import wrapped_partial
+from .config import ArrayLike, jit, jit_kwargs, NDArray, xp
 
-from .config import jit, xp, NDArray
 
-
-# TODO docstring
-@jit
-def _splint(data: NDArray, x: NDArray) -> NDArray:
-    """
-    Perform cubic spline interpolation to calculate an interpolated y value at
-    a given x value.
-
+@wrapped_partial(jit, **jit_kwargs)
+def _splint(data: NDArray, x: ArrayLike) -> NDArray:
+    """_summary_
 
     Parameters
     ----------
-    data : xp.ndarray
-        Array with the known x, y, and y'' stacked along
-        the first axis.
-    x : float
-        Target x value at which to calculate the interpolated y value.
+    data : NDArray
+        _description_
+    x : ArrayLike
+        _description_
 
     Returns
     -------
-    float
-        Interpolated y value at the target x value
+    NDArray
+        _description_
     """
-    # klo = xp.apply_along_axis(lambda a: a.searchsorted(x), axis=-1, arr=data[..., 0, :]) - 1
-    # TODO N-D splint function where multiple elements are queried at once
-    # klo = xp.apply_along_axis(lambda a: a.searchsorted(x), axis = 1, arr = data[0]) - 1
-    klo = xp.searchsorted(data[0], x, 'right') - 1
-    # klo = data[0].searchsorted(x) - 1
-    # print(data.shape)
-    # klo = xp.apply_along_axis(lambda a: xp.searchsorted(a, x), axis=0, arr = data)
-    # print(klo)
-    # print(data[..., :, :].shape)
-    # print(data[..., 0, :][klo])
-    
-    # output = xp.zeros_like(klo)
-    # for index in xp.ndindex(klo.shape[:-x.ndim]):
-    #     print(index)
-    #     print(output[index].shape)
-    #     print(data[(*index, 0)].shape)
-    #     output[index] = data[(*index, 0)][klo[index]]
-    # print(klo)
-    # print(output)
-    #     # print(index)
-    # print(klo.shape)
-    # print('hola', data[..., 0, klo].shape)
-    # print(data[..., 0, :].shape)
-    # print(data[0, 0][klo[0]])
-    # print(data[1, 0][klo[1]])
-    # print(xp.take(data[..., 0, :], klo, axis=-2 ))
-    # raise Exception
+    x = xp.asarray(x)
+    klo = xp.apply_along_axis(
+        lambda y: xp.searchsorted(y, x, side="right") - 1,
+        axis=-1,
+        arr=data[..., 0, :],
+    )  # "right" - 1 # TODO try out different combos
 
-    h = data[..., 0, :][klo + 1] - data[..., 0, :][klo]
-    # print('hello', data[..., 0, :][klo])
+    indices = xp.indices(klo.shape)[: data.ndim - 2]
+    h = data[..., 0, :][(*indices, klo + 1)] - data[..., 0, :][(*indices, klo)]
+    # ??? should i do this instead? avoid error warnings
+    # h = xp.where(~xp.isnan(h), h, xp.nan)
+    a = (data[..., 0, :][(*indices, klo + 1)] - x) / h
+    b = (x - data[..., 0, :][(*indices, klo)]) / h
 
-    # TODO check if this is actually necessary
-    # if h == 0.0:
-    #     return (data[1][klo] + data[1][klo + 1]) / 2.0
+    d1_b = data[..., 1, :][(*indices, klo)]
+    d1_a = data[..., 1, :][(*indices, klo + 1)]
 
-    a = (data[..., 0, :][klo + 1] - x) / h
-    b = (x - data[..., 0, :][klo]) / h
-    return (
-        a * data[..., 1, :][klo]
-        + b * data[..., 1, :][klo + 1]
-        + (
-            (a * a * a - a) * data[..., 2, :][klo]
-            + (b * b * b - b) * data[..., 2, :][klo + 1]
-        )
-        * (h * h)
-        / 6.0
+    d2_b = data[..., 2, :][(*indices, klo)]
+    d2_a = data[..., 2, :][(*indices, klo + 1)]
+    output = (
+        a * d1_b
+        + b * d1_a
+        + ((a * a * a - a) * d2_b + (b * b * b - b) * d2_a) * (h * h) / 6.0
     )
+    # TODO where h == 0
+    # return output
+    # TODO check this works
+    if xp.__name__ == "jax.numpy":
+        return xp.where(h != 0, output, (d1_a + d1_b) / 2)
+    output = xp.asarray(output)
+    output[h == 0] = (d1_a + d1_b) / 2
+    return output
