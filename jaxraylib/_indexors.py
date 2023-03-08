366dc0f4630be9dce17004b3e1eba9d3c5401f73
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-03-08 15:00:40 +0100

    refactor to use common function

---
+++ jaxraylib/_indexors.py
@@ -0,0 +1,25 @@
+"""_summary_
+"""
+# TODO type hinting
+# TODO docstring
+
+from ._utilities import wrapped_partial
+from .config import xp, jit, jit_kwargs
+
+
+@wrapped_partial(jit, **jit_kwargs)
+def _index1d(data, a):
+    _a = xp.asarray(a)
+    condition = (_a >= 0) & (_a < data.shape[0])
+    output = data[xp.where(condition, _a, 0)]
+    return xp.where(condition, output, xp.nan)
+
+
+@wrapped_partial(jit, **jit_kwargs)
+def _index2d(data, a, b):
+    _a = xp.asarray(a).reshape((*a.shape, *(1,) * b.ndim))
+    _b = xp.asarray(b).reshape((*(1,) * a.ndim, *b.shape))
+    condition_a = (_a >= 0) & (_a < data.shape[0])
+    condition_b = (_b >= 0) & (b < data.shape[1])
+    output = data[xp.where(condition_a, _a, 0), xp.where(condition_b, _b, 0)]
+    return xp.where(condition_a & condition_b, output, xp.nan)
