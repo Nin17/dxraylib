ce479d5c8ed4b4a8bce156498a3c9e2edf3fe176
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-03-11 14:50:55 +0100

    add Atomic number to symbol

--- dxraylib/xraylib_parser.py
+++ dxraylib/xraylib_parser.py
@@ -22,3 +22,19 @@ def CompoundParser(compoundString: str) -> dict:
         _description_
     """
     return xraylib.CompoundParser(compoundString)
+
+
+def AtomicNumberToSymbol(Z: int) -> dict:
+    """_summary_
+
+    Parameters
+    ----------
+    Z : int
+        _description_
+
+    Returns
+    -------
+    dict
+        _description_
+    """
+    return xraylib.AtomicNumberToSymbol(Z)
