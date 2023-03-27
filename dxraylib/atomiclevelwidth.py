95c5a71f260afd48ad98bb858456520442f708c0
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-03-27 19:24:47 +0200

    docstring & type hinting

--- dxraylib/atomiclevelwidth.py
+++ dxraylib/atomiclevelwidth.py
@@ -1,13 +1,13 @@
-"""_summary_
 """
-# TODO docstring
+Atomic level widths
+"""
 
 from __future__ import annotations
 import os
 
 from ._indexors import _index2d
 from ._utilities import asarray, wrapped_partial
-from .config import ArrayLike, jit, jit_kwargs, NDArray, xp
+from .config import Array, ArrayLike, jit, jit_kwargs, xp
 
 _DIRPATH = os.path.dirname(__file__)
 _ALW_PATH = os.path.join(_DIRPATH, "data/atomic_level_width.npy")
@@ -16,20 +16,21 @@ _ALW = xp.load(_ALW_PATH)
 
 @wrapped_partial(jit, **jit_kwargs)
 @asarray()
-def AtomicLevelWidth(Z: ArrayLike, shell: ArrayLike) -> NDArray:
-    """_summary_
+def AtomicLevelWidth(Z: ArrayLike, shell: ArrayLike) -> Array:
+    """
+    Atomic level width (keV).
 
     Parameters
     ----------
-    Z : ArrayLike
+    Z : array_like
         atomic number
-    shell : ArrayLike
-        _description_
+    shell : array_like
+        shell-type macro
 
     Returns
     -------
-    NDArray
-        _description_
+    array
+        atomic level width (keV)
     """
 
     return _index2d(_ALW, Z - 10, shell)
