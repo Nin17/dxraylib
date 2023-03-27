5f4a24483a997608a0aaf8c219b98e4787213ec0
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-03-27 18:34:43 +0200

    actually do some documenting

---
+++ docs/build/_sources/api/non_rad_rate.rst.txt
@@ -0,0 +1,9 @@
+Non-radiative rates
+===================
+
+`Relevant section in the xraylib documentation <xraylib_>`_.
+
+.. _xraylib: https://github.com/tschoonj/xraylib/wiki/The-xraylib-API-list-of-all-functions#non-radiative-rates
+
+.. autofunction:: dxraylib.AugerRate
+    
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,60 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'JAXraylib'
-copyright = '2022, Chris Ninham'
-author = 'Chris Ninham'
-release = '0.0.1'
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../dxraylib"))
+sys.path.insert(1, os.path.abspath("../../dxraylib/_utilities.py"))
+print(sys.path)
+
+project = "dxraylib"
+copyright = "2023, Chris Ninham"
+author = "Chris Ninham"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+latex_engine = "pdflatex"
+latex_elements = {
+    "extraclassoptions": "openany,oneside",
+    "preamble": r"""
+\DeclareUnicodeCharacter{0394}{$\Delta$}
+\DeclareUnicodeCharacter{212B}{$\textup{~\AA}$}
+\DeclareUnicodeCharacter{207B}{$^{-}$}
+\DeclareUnicodeCharacter{03C0}{$\pi$}
+\DeclareUnicodeCharacter{03B8}{$\theta$}
+\DeclareUnicodeCharacter{03BB}{$\lambda$}
+\DeclareUnicodeCharacter{03B2}{$\beta$}
+\DeclareUnicodeCharacter{1D7AD}{$\delta$}
+""",
+}
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
