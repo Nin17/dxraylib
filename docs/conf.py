3d3dc4b57d04c7fa7862741140d052563d668efc
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-04-07 17:09:24 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../../dxraylib"))
 sys.path.insert(1, os.path.abspath("../../dxraylib/_utilities.py"))
-print(sys.path)
+# print(sys.path)
 
 project = "dxraylib"
 copyright = "2023, Chris Ninham"
@@ -21,7 +21,7 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.napoleon", 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -30,8 +30,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = "sphinx_book_theme"
+# html_static_path = ["_static"]
 
 latex_engine = "pdflatex"
 latex_elements = {
@@ -63,3 +63,24 @@ napoleon_use_rtype = True
 napoleon_preprocess_types = False
 napoleon_type_aliases = None
 napoleon_attr_annotations = True
+
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This patterns also effect to html_static_path and html_extra_path
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
