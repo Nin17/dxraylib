38551a45ae91d0e560d664d12a7f18c1d4e1378a
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-05-04 09:12:28 +0200

    move new datasets here rewrite load to cache and respond to changes in config

--- dxraylib/_load.py
+++ dxraylib/_load.py
@@ -8,10 +8,12 @@ helper function to load the data
 # TODO document & type hint
 
 import os
+import functools
 
 import numpy as np
 
-from .config import xp
+# from .config import xp
+from . import config as cfg
 
 # _DIRPATH = os.path.dirname(__file__)
 
@@ -20,16 +22,52 @@ _PATH = os.path.join(os.path.dirname(__file__), "data/data.npz")
 _DATA = np.load(_PATH)
 
 
-def _load(file):
-    if xp.__name__ == "mygrad":
+# TODO remove from here
+_DATA = dict(_DATA)
+
+_DATA["compton_profiles"] = np.load(
+    "/Users/chris/Documents/PhD/Simulations/dxraylib69/dxraylib/data/compton_profiles.npy"
+)
+
+_DATA["compton_profiles_partial"] = np.load(
+    "/Users/chris/Documents/PhD/Simulations/dxraylib69/dxraylib/data/compton_profiles_partial.npy"
+)
+
+_DATA["electron_config"] = np.load(
+    "/Users/chris/Documents/PhD/Simulations/dxraylib69/dxraylib/data/electron_config.npy"
+)
+
+_DATA["kissel_pe"] = np.load(
+    "/Users/chris/Documents/PhD/Simulations/dxraylib69/dxraylib/data/kissel_pe.npy"
+)
+
+
+# TODO remove to here
+
+
+# Not functools.cache for compatibility with python 3.8
+@functools.lru_cache(maxsize=None)
+def _load(file, xp=cfg.xp):
+    # TODO new functions here and then incoporate in data.npz
+
+    if xp.__name__ == "mygrad":  # ???
         return _DATA[file]
-    else:
-        return xp.asarray(_DATA[file])
+    if xp.__name__ == "torch":
+        # torch.searchsorted doesn't like nans
+        # FIXME need to keep nans if it is indexing not interpolating
+        # TODO think of condition, maybe axis[-2] of size 3
+        return xp.nan_to_num(xp.tensor(_DATA[file]), nan=float("inf"))
+
+    return xp.asarray(_DATA[file])
     # return (
     #     xp.asarray(_DATA[file])
     #     if xp.__name__ != "mygrad"
     #     else xp.tensor(_DATA[file])
     # )
+
+
+def load(file):
+    return _load(file, cfg.xp)
 
 
 # def _load(file):
